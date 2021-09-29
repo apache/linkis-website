fac6d066d28e07e8cab54cdc0bc479d2cf753124
lucaszhu <lucaszhu@webank.com>
2021-09-29 16:50:05 +0800

    FIX ROUTER

--- src/router.js
+++ src/router.js
@@ -1,16 +1,33 @@
-const routes = [
-  {
+const routes = [{
     path: '/',
-    component: () => import(/* webpackChunkName: "group-app" */ './app.vue'),
-    children: [
-      { path: '', name: 'home', component: () => import(/* webpackChunkName: "group-home" */ './pages/home.vue') },
-      { path: 'docs', name: 'docs', component: () => import(/* webpackChunkName: "group-docs" */ './pages/docs.vue') },
-      { path: 'faq', name: 'faq', component: () => import(/* webpackChunkName: "group-faq" */ './pages/faq.vue') },
-      { path: 'download', name: 'download', component: () => import(/* webpackChunkName: "group-download" */ './pages/download.vue') },
-      { path: 'blog', name: 'blog', component: () => import(/* webpackChunkName: "group-blog" */ './pages/blog.vue') },
-      { path: 'team', name: 'team', component: () => import(/* webpackChunkName: "group-team" */ './pages/team.vue') },
-    ]
-  }
+    name: 'home',
+    component: () => import( /* webpackChunkName: "group-home" */ './pages/home.vue')
+  },
+  {
+    path: '/docs',
+    name: 'docs',
+    component: () => import( /* webpackChunkName: "group-docs" */ './pages/docs.vue')
+  },
+  {
+    path: '/faq',
+    name: 'faq',
+    component: () => import( /* webpackChunkName: "group-faq" */ './pages/faq.vue')
+  },
+  {
+    path: '/download',
+    name: 'download',
+    component: () => import( /* webpackChunkName: "group-download" */ './pages/download.vue')
+  },
+  {
+    path: '/blog',
+    name: 'blog',
+    component: () => import( /* webpackChunkName: "group-blog" */ './pages/blog.vue')
+  },
+  {
+    path: '/team',
+    name: 'team',
+    component: () => import( /* webpackChunkName: "group-team" */ './pages/team.vue')
+  },
 ]
 
 export default routes;
