fa8bab824f7be62c5869ca46007fc85534977055
lucaszhu <lucaszhu@webank.com>
2021-09-29 11:21:09 +0800

    ADD FOOTER

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="nav">
+<div>
+  <nav class="nav">
     <div class="ctn-block">
       <div class="nav-logo">
         Apache Linkis
@@ -21,7 +21,36 @@ import HelloWorld from './components/HelloWorld.vue'
         <div class="menu-item">Language</div>
       </div>
     </div>
-  </div>
+  </nav>
+  <router-view/>
+  <footer class="footer">
+    <div class="ctn-block">
+      <div class="footer-links-row">
+        <div class="footer-links">
+          <h3 class="links-title">Linkis</h3>
+          <a href="" class="links-item">Documentation</a>
+          <a href="" class="links-item">Events</a>
+          <a href="" class="links-item">Releases</a>
+        </div>
+        <div class="footer-links">
+          <h3 class="links-title">Community</h3>
+          <a href="" class="links-item">GitHub</a>
+          <a href="" class="links-item">Issue Tracker</a>
+          <a href="" class="links-item">Pull Requests</a>
+        </div>
+        <div class="footer-links">
+          <h3 class="links-title">Apache Software Foundation</h3>
+          <a href="" class="links-item">Foundation</a>
+          <a href="" class="links-item">License</a>
+          <a href="" class="links-item">Sponsorship</a>
+          <a href="" class="links-item">Thanks</a>
+        </div>
+      </div>
+      <p class="footer-desc">Apache Linkis (Incubating) is an effort undergoing incubation at The Apache Software Foundation, sponsored by the Apache Incubator. Incubation is required of all newly accepted projects until a further review indicates that the infrastructure, communications, and decision making process have stabilized in a manner consistent with other successful ASF projects. While incubation status is not necessarily a reflection of the completeness or stability of the code, it does indicate that the project has yet to be fully endorsed by the ASF.</p>
+      <p class="footer-desc text-center">Copyright © 2021 The Apache Software Foundation. Apache Linkis, Apache Incubator, Linkis, Apache, the Apache feather logo, the Apache<br>Linkis logo and the Apache Incubator project logo are trademarks of The Apache Software Foundation.</p>
+    </div>
+  </footer>
+</div>
 </template>
 
 <style lang="less">
@@ -51,6 +80,7 @@ import HelloWorld from './components/HelloWorld.vue'
   .menu-list{
     flex: 1;
     display: flex;
+    justify-content: flex-end;
     .menu-item{
       margin-left: 16px;
       margin-right: 16px;
@@ -62,6 +92,34 @@ import HelloWorld from './components/HelloWorld.vue'
       &.router-link-active{
         color: @active-color;
         border-color: @active-color;
+      }
+    }
+  }
+}
+.footer{
+  padding-top: 40px;
+  background: #F9FAFB;
+  .footer-desc{
+    padding: 0 20px 30px;
+    color: #999999;
+    font-weight: 400;
+  }
+  .footer-links-row{
+    display: flex;
+    font-size: 16px;
+    .footer-links{
+      flex: 1;
+      padding: 20px;
+      .links-title{
+        margin-bottom: 16px;
+      }
+      .links-item{
+        display: block;
+        margin-bottom: 10px;
+        color: rgba(15,18,34,0.65);
+        &:hover{
+          text-decoration: underline;
+        }
       }
     }
   }
