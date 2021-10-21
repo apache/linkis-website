114979c0ce4925cc16606ea7c4e173c0ccbcd014
casionone <casionone@gmail.com>
2021-10-21 16:37:41 +0800

    update version

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Markdown from 'vite-plugin-md'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
