10bda3a727afaaf07adabea8fb15c9fb5b726bf5
Beacontownfc <89081023+Beacontownfc@users.noreply.github.com>
2022-07-06 22:31:38 +0800

    Update main.js

--- .github/actions/Auto_close_associate_issue/main.js
+++ .github/actions/Auto_close_associate_issue/main.js
@@ -2,6 +2,14 @@ let body = process.env['INPUT_PRBODY'];
 
 let pattern = /#\d+/;
 
-let issueNumber = body.match(pattern)[0].replace('#', '');
+let issueNumber;
+
+try {
+    issueNumber = body.match(pattern)[0].replace('#', '');
+} catch{
+    issueNumber = -1;
+}
+
 
 console.log(`::set-output name=issueNumber::${issueNumber}`);
+
