dba177f1b8cfaff1b2f85054239aad5b8f092129
Beacontownfc <1963697267@qq.com>
2022-07-13 10:24:29 +0800

    Fix Redirection

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -340,6 +340,19 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         },
       },
     ],
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        createRedirects(existingPath) {
+          if (existingPath.includes('/1.1.2')) {
+            return [
+              existingPath.replace('/1.1.2', '/latest'),
+            ];
+          }
+          return undefined; // Return a false value: no redirect created
+        },
+      }
+    ]
     //    [
     //      '@docusaurus/plugin-content-docs',
     //      {
