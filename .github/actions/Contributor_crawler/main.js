fc7bdb8ea5cda0b56f2991b90cab1e817463b8fd
Beacontownfc <89081023+Beacontownfc@users.noreply.github.com>
2022-10-11 15:41:06 +0800

    Add an crawler action to workflow (#514)
    * add crawler
    
    * Update scheduled-crawl.yml

---
+++ .github/actions/Contributor_crawler/main.js
@@ -0,0 +1,26 @@
+const https = require('https');
+const fs = require('fs');
+let num = process.env['INPUT_NUM'];
+let repo = process.env['INPUT_REPO'];
+
+let name = repo == 'apache/incubator-linkis-website' ?  'contributorsWebsite.json' : 'contributors.json'
+const options = {headers: {'User-Agent': 'request'}};
+
+// repo such as apache/incubator-linkis-website
+https.get(`https://api.github.com/repos/${repo}/contributors?page=1&per_page=${num}`, options, res => {
+    let chunks = [];
+    res.on('data', chunk => {
+        chunks.push(chunk);
+    });
+    res.on('end', () => {
+        let jsonstr = Buffer.concat(chunks).toString('utf-8');
+        fs.writeFileSync(`./src/components/${name}`, jsonstr, (err) => {
+            if(err) console.log(err);
+        })
+    })
+}).on('error', err => {
+    console.log('Error: ', err.message);
+});
+
+
+
