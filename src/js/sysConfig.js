4c29df7756397c35d4feac64209307c6c880697d
peacewong <peacewong@users.noreply.github.com>
2025-01-08 08:28:17 +0000

    deploy: 5cba103742531c1626352cd0738102b9d86acdf5

--- src/js/sysConfig.js
+++
@@ -1,9 +0,0 @@
-const  systemConfiguration={
-    github:{
-        "projectUrl":"https://github.com/apache/linkis",
-        "projectReleaseUrl":"https://github.com/apache/linkis/releases",
-        "projectIssueUrl":"https://github.com/apache/linkis/issues",
-        "projectPrUrl":"https://github.com/apache/linkis/pulls",
-    },
-}
-export  default systemConfiguration
